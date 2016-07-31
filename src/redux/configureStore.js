684a4aa3dc4c31047cb19335f8044ba0e1e82d3a
Valjean Clark III <valjeanbud@gmail.com>
2016-07-31 15:03:18 -0700

    package upgrades, remove react-router-redux

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,10 @@
 import { applyMiddleware, compose, createStore } from 'redux'
-import { syncHistory } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 
-export default function configureStore ({ initialState = {}, history }) {
-  // Sync with router via history instance (main.jsx)
-  const routerMiddleware = syncHistory(history)
-
+export default function configureStore ({initialState = {}}) {
   // Compose final middleware and use devtools in debug environment
-  let middleware = applyMiddleware(thunk, routerMiddleware)
+  let middleware = applyMiddleware(thunk)
   if (__DEBUG__) {
     const devTools = window.devToolsExtension
       ? window.devToolsExtension()
@@ -16,9 +12,8 @@ export default function configureStore ({ initialState = {}, history }) {
     middleware = compose(middleware, devTools)
   }
 
-  // Create final store and subscribe router in debug env ie. for devtools
+  // Create final store
   const store = middleware(createStore)(rootReducer, initialState)
-  if (__DEBUG__) routerMiddleware.listenForReplays(store, ({ router }) => router.location)
 
   if (module.hot) {
     module.hot.accept('./rootReducer', () => {

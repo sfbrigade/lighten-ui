5c33de38513da27c75c554d05a17c54ae8fca856
sahilshah1 <s.shah1357@gmail.com>
2016-08-14 14:28:59 -0700

    Added login view that sends request to api, no state saved based on outcome yet

---
+++ src/views/Login.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import http from 'superagent'
+
+export class Login extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      user: '',
+      password: ''
+    }
+  }
+
+  setUser = e => {
+    this.state.user = e.target.value
+  }
+
+  setPassword = e => {
+    this.state.password = e.target.value
+  }
+
+  // This will be called when the user clicks on the login button
+  login = e => {
+    e.preventDefault()
+
+    http
+      .post('/api-token-auth/')
+      .send({ 'username': this.state.user, 'password': this.state.password })
+      .end((error, response) => {
+        if (error) {
+          return console.error(error)
+        }
+        return console.log(response)
+      })
+  }
+
+  render () {
+    return (
+      <div>
+        <form role="form">
+          <div className="form-group">
+            <input type="text" placeholder="Username" defaultValue={this.state.user}
+              onChange={this.setUser} />
+            <input type="password" placeholder="Password"
+              onChange={this.setPassword} />
+          </div>
+          <button type="submit" onClick={this.login}>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Login

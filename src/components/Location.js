832b8473345ac8c9e3916ce5d0e929651cf8fbbf
Valjean Clark <valjeanbud@gmail.com>
2016-12-11 13:41:10 -0800

    do not return location if it does not have address

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,5 +1,9 @@
 import React, { PropTypes } from 'react'
+import _get from 'lodash.get'
+
 import DataBlock from './DataBlock'
+
+const addressKey = 'physical_address'
 
 export default class Location extends React.Component {
 
@@ -11,17 +15,20 @@ export default class Location extends React.Component {
 
   render () {
     const {location} = this.props
+    const address = _get(location, addressKey)
+
+    if (!address) return null
 
     const googleMapsIframeUrl = [
       'https://www.google.com/maps/embed/v1/place',
       '?key=AIzaSyCLeuAb-KkMbNrB489TN77duPkdsqHCTco',
       '&zoom=14',
-      `&q=${encodeURIComponent(Location.buildAddressString(location.physical_address))}`
+      `&q=${encodeURIComponent(Location.buildAddressString(address))}`
     ].join('')
 
     return (
       <div className="Location">
-        {this.renderPhysicalAddress(location.physical_address)}
+        {this.renderPhysicalAddress(address)}
         <iframe
           width="600"
           height="450"
@@ -45,7 +52,7 @@ export default class Location extends React.Component {
 
   onSave = (key) => (value) => {
     const {path, onSave} = this.props
-    onSave(`${path}.physical_address.${key}`)(value)
+    onSave(`${path}.${addressKey}.${key}`)(value)
   }
 
   static buildAddressString = ({street, city, state, zipcode, country}) => `${street} ${city} ${state} ${zipcode} ${country}`
